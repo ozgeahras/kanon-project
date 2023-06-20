22a94887c08ac41e90911e19b01c9c0309e2fcc8
ozgeahras <ozgeahras@gmail.com>
2023-06-20 15:59:56 +0300

    created GamePAge.jsx, css files for spinning game

---
+++ src/GamePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GamePage() {
+  return (
+    <div>
+      <h1>Game Page</h1>
+      <p>This is the game page content.</p>
+    </div>
+  );
+}
+
+export default GamePage;
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar() {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li className="navbar-item">Home</li>
-        <li className="navbar-item">Play Game</li>
-        <li className="navbar-item">Login</li>
+        <li className="navbar-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/new-game">Play Game</Link>
+        </li>
       </ul>
     </nav>
   );
