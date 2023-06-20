7c50265b7adbaf74e9e23cfdd6c8c68a5258411c
ozgeahras <ozgeahras@gmail.com>
2023-06-20 14:04:29 +0300

    added SearchBox.css for the component

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import './SearchBox.css';
 
 function SearchBox({ handleSearch }) {
   const handleInputChange = (event) => {
@@ -7,8 +10,18 @@ function SearchBox({ handleSearch }) {
   };
 
   return (
-    <div>
-      <input type="text" onChange={handleInputChange} placeholder="Search..." />
+    <div className="searchbox-container">
+      <div className="searchbox-wrapper">
+        <input
+          className="searchbox-input"
+          type="text"
+          onChange={handleInputChange}
+          placeholder="Search..."
+        />
+        <div className="searchbox-icon">
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
+      </div>
     </div>
   );
 }
