849b1c459b47c2012babe8b9a6485f2ce023876a
ozgeahras <ozgeahras@gmail.com>
2023-06-20 11:41:49 +0300

    added search functionality to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
 import dataStore from './dataStore';
 import './App.css';
+import SearchBox from './SearchBox';
 
 const App = observer(() => {
+  const [filteredData, setFilteredData] = useState([]);
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
     dataStore.fetchData();
   }, []);
 
+  const handleSearch = (query) => {
+    setQuery(query);
+    const filtered = dataStore.data.filter((item) =>
+      item.title.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+
+  const renderData = query ? filteredData : dataStore.data;
+
   return (
     <div className="container">
-      {dataStore.data.map((item) => (
+      <SearchBox handleSearch={handleSearch} />
+      {renderData.map((item) => (
         <div key={item.id} className="card">
           <p>Title: {item.title}</p>
           <p>Provider: {item.providerName}</p>
